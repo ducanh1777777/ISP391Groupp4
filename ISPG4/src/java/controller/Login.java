c480f04060bcff2f5fb8afc59db17dc892c83654
ducanh1777777 <cowsleep17@gmail.com>
2024-03-03 02:26:38 +0700

    sua lai Login.java

--- ISPG4/src/java/controller/Login.java
+++ ISPG4/src/java/controller/Login.java
@@ -9,6 +9,7 @@ import entity.Users;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import jakarta.servlet.http.HttpSession;
  *
  * @author Admin
  */
+@WebServlet(name = "Login", urlPatterns = {"/login"})
 public class Login extends HttpServlet {
 
     /**
